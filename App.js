76fc7e49226f369b8c9dafc8076b35132b9dda61
Nicholas Ferrara <nferrara100@gmail.com>
2019-09-18 17:18:00 +0100

    feat: added side menu and safe area disappeared

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import SideMenu from "react-native-side-menu";
 
 export default function App() {
+  const menu = <View style={{backgroundColor: "blue"}}><Text>I'm in the menu</Text></View>
   return (
       <SafeAreaView style={{backgroundColor: "red", flex: 1}}>
+        <SideMenu menu={menu}>
           <View style={[styles.container, {backgroundColor: "white"}]}>
               <Text>Open up App.js to start working on your app!</Text>
           </View>
+        </SideMenu>
       </SafeAreaView>
   );
 }
